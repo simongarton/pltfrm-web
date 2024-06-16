a339993c42824fbc1f9cc3c362cd3031ad3e17b3
Simon Garton <simon.garton@gmail.com>
2024-06-16 17:41:59 +1200

    More charts, but having to add lots of delays ?

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Weather from "./components/Weather";
 import RainHourLineChart from "./components/RainHourLineChart";
 import HourForecastChart from "./components/HourForecastChart";
+import HourForecastChart2 from "./components/HourForecastChart2";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <Weather />
       <RainHourLineChart />
       <HourForecastChart />
+      <HourForecastChart2 />
     </div>
   );
 }
--- src/components/HourForecastChart2.jsx
+++ src/components/HourForecastChart2.jsx
@@ -9,7 +9,7 @@ const API_KEY = process.env.PLTFRM_API_KEY;
 // eslint-disable-next-line no-undef
 const URL = process.env.PLTFRM_URL;
 
-function HourForecastChart() {
+function HourForecastChart2() {
   const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
@@ -32,12 +32,11 @@ function HourForecastChart() {
       .then((data) => {
         setChartData(data);
       });
-    }, 4000)},[]);
+    }, 6000)},[]);
 
   const labels = [];
-  const temps = [];
-  const feels_likes = [];
-  const dew_points = [];
+  const pressure = [];
+  const humidity = [];
 
   console.log(chartData);
 
@@ -45,9 +44,8 @@ function HourForecastChart() {
     for (var i = 0; i < chartData["forecastHours"].length; i++) {
       var date = new Date(chartData["forecastHours"][i]["dt"]*1000);
       labels.push(date.getHours());
-      temps.push(chartData["forecastHours"][i]["temp"]);
-      feels_likes.push(chartData["forecastHours"][i]["feels_like"]);
-      dew_points.push(chartData["forecastHours"][i]["dew_point"]);
+      pressure.push(chartData["forecastHours"][i]["pressure"]);
+      humidity.push(chartData["forecastHours"][i]["humidity"]);
     }
   }
 
@@ -64,6 +62,23 @@ function HourForecastChart() {
         display: true,
         text: title,
       },
+      scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      }
     },
   };
 
@@ -71,19 +86,16 @@ function HourForecastChart() {
     labels,
     datasets: [
       {
-        label: "Temperature",
-        data: temps,
-        backgroundColor: transparentize(CHART_COLORS.blue, 0.2),
+        label: "Pressure",
+        data: pressure,
+        backgroundColor: transparentize(CHART_COLORS.red, 0.2),
+        yAxisID: 'y'
       },
       {
-        label: "Feels like",
-        data: feels_likes,
-        backgroundColor: transparentize(CHART_COLORS.green, 0.2),
-      },
-      {
-        label: "Dew point",
-        data: dew_points,
-        backgroundColor: transparentize(CHART_COLORS.purple, 0.2),
+        label: "Humidity",
+        data: humidity,
+        backgroundColor: transparentize(CHART_COLORS.orange, 0.2),
+        yAxisID: 'y'
       },
     ],
   };
@@ -92,4 +104,4 @@ function HourForecastChart() {
       <LineChart options={options} data={data} />
   );
 }
-export default HourForecastChart;
+export default HourForecastChart2;
